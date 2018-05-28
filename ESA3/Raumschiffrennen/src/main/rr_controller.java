cd65ced2d3798fdcdacb1f1d18798cd3b5e1cb0a
Mathias Sotta <22378688+MathiasSotta@users.noreply.github.com>
2018-05-28 21:32:15 +0200

    Added files for ESA3

---
+++ ESA3/Raumschiffrennen/src/main/rr_controller.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+
+import java.util.ResourceBundle;
+
+public class rr_controller implements Initializable {
+
+    @FXML
+    private Rectangle rsB;
+
+    @FXML
+    private Rectangle rsY;
+
+    @FXML
+    private Rectangle rsR;
+
+    @FXML
+    private Text statusText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
